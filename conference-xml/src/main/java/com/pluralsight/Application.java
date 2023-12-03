77caa5af74c74edaea2b91a577edd8740ab8140f
sachsriv <srivastava.mails@gmail.com>
2023-12-03 13:49:48 +0530

    conference-xml - xml based configuration

---
+++ conference-xml/src/main/java/com/pluralsight/Application.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import com.pluralsight.service.SpeakerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args){
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpeakerService service = appContext.getBean("speakerService",SpeakerService.class);
+        System.out.println(service.findAll().get(0).getFirstName());
+        //System.out.println(service.findAll().get(0).getLastName());
+        //service = appContext.getBean("speakerService",SpeakerService.class);
+        //System.out.println(service);
+
+    }
+}
---
+++ conference-xml/src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,24 @@
+package com.pluralsight.model;
+
+public class Speaker {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+
+}
